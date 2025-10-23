b2f6d8fd57ac2fbb05685abe97e8446b18ff7a1a
sihyun10 <hyhy200170@gmail.com>
2025-10-23 22:45:32 +0900

    feat(Cars): 자동차 집합의 관리 클래스 구현
    Cars 클래스는 여러 대의 자동차(Car)를 관리하고,
    모든 자동차의 전진과 우승자 계산을 수행한다.
    moveAll()을 통해 각 차들을 전진 시키고, findWinners()를 통해 우승자를 추출해준다.
    우승자 추출은 각 차들의 거리들 중 최대값을 구하여 그 거리와 같은 자동차 목록을 추출하는것이다.

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,34 @@
+package racingcar.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.util.RandomNumberGenerator;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(List<String> names) {
+        this.cars = names.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public void moveAll(RandomNumberGenerator generator) {
+        cars.forEach(car -> car.move(generator.generate()));
+    }
+
+    public int findMaxDistance() {
+        return cars.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
+    }
+
+    public List<Car> findWinners() {
+        int maxDistance = findMaxDistance();
+        return cars.stream()
+                .filter(car -> car.getDistance() == maxDistance)
+                .collect(Collectors.toList());
+    }
+}
