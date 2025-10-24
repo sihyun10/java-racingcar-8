d00a8b8bda286c9390ba58626325326f19066fc6
sihyun10 <hyhy200170@gmail.com>
2025-10-24 19:50:04 +0900

    test(RacingCountValidatorTest): 시도 횟수 검증 로직 테스트
    정상 로직과 예외 발생하는 로직 테스트를 작성하였다.
    정상 시도 횟수를 입력하면 통과한다.
    예외발생은 먼저 시도 횟수가 숫자가 아닌 입력일 경우 예외 발생한다. (문자열, 빈 문자열, 음수, 특수문자 등)
    시도 횟수가 0일 경우 예외 발생한다. 시도 횟수가 우리가 정해둔 20회 이상을 입력하게 되면 예외 발생한다.

--- src/test/java/racingcar/validator/RacingCountValidatorTest.java
+++ src/test/java/racingcar/validator/RacingCountValidatorTest.java
@@ -1,4 +1,60 @@
 package racingcar.validator;
 
-public class RacingCountValidator {
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.exception.ErrorMessage;
+import racingcar.exception.InvalidInputException;
+
+class RacingCountValidatorTest {
+
+    @Nested
+    @DisplayName("정상 시도 횟수 검증")
+    class ValidCountTest {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"1", "5", "20"})
+        @DisplayName("1에서 20 사이 입력일 경우 통과")
+        void 횟수_1에서_20_사이_입력_통과(String input) {
+            RacingCountValidator.validate(input);
+        }
+    }
+
+    @Nested
+    @DisplayName("시도 횟수 숫자 검증")
+    class NumberValidationTest {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"테스트", "a", "3.5", "!", "##", "-1", " "})
+        @DisplayName("숫자가 아닌 입력일 경우 예외 발생")
+        void 숫자가_아닌_입력_예외_발생(String input) {
+            assertThatThrownBy(() -> RacingCountValidator.validate(input))
+                    .isInstanceOf(InvalidInputException.class)
+                    .hasMessage(ErrorMessage.INVALID_NUMBER.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("시도 횟수가 0일 경우 예외 발생")
+    void 시도_횟수_0_예외_발생() {
+        String input = "0";
+
+        assertThatThrownBy(() -> RacingCountValidator.validate(input))
+                .isInstanceOf(InvalidInputException.class)
+                .hasMessage(ErrorMessage.ZERO_NUMBER.getMessage());
+    }
+
+    @Test
+    @DisplayName("시도 횟수 최대값 검증")
+    void 최대_횟수_초과_예외_발생() {
+        String input = "21";
+
+        assertThatThrownBy(() -> RacingCountValidator.validate(input))
+                .isInstanceOf(InvalidInputException.class)
+                .hasMessage(ErrorMessage.EXCEED_MAX_COUNT.getMessage());
+    }
 }
